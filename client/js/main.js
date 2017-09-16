977d2b2c02fbb1969ee0edd4a270a416e31026c1
Roy Barber <roy@wearedamage.com>
2017-09-16 14:06:08 +0100

    File upload merge and correct SpeedTest code

--- client/js/main.js
+++ client/js/main.js
@@ -3,4 +3,12 @@ $(function(){
 		doc = $(document),
 		body = $('body'),
 		$self = $(this);
+		
+	// File upload
+    $('.file-upload').change(function() {
+        var filepath = this.value;
+        var m = filepath.match(/([^\/\\]+)$/);
+        var filename = m[1];
+        $(this).parent('label').siblings('span').html(filename);
+    });
 });
