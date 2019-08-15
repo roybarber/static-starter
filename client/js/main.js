478fd1402446fdd3fb50eca22ae7348d8c0c558d
Roy Barber <hi@roybarber.com>
2019-08-15 16:19:35 +0100

    Base JS

--- client/js/main.js
+++ client/js/main.js
@@ -1,24 +1,5 @@
 $(function(){
 		
-	// Lazy Load - https://github.com/ApoorvSaxena/lozad.js
-    var observer = lozad('.lozad', {
-        threshold: 0.1,
-        load: function(el) {
-            el.src = el.getAttribute("data-src");
-            el.onload = function() {
-	          el.classList.add('loaded');
-              console.log("Success " + el.localName.toUpperCase() + " " + el.getAttribute("data-index") + " lazy loaded.")
-            }
-        }
-    })
-    observer.observe()
-	
-	// File upload
-    $('.file-upload').change(function() {
-        var filepath = this.value;
-        var m = filepath.match(/([^\/\\]+)$/);
-        var filename = m[1];
-        $(this).parent('label').siblings('span').html(filename);
-    });
+	console.log('JS Ready');
     
 });
