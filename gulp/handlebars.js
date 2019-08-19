d495709c6e4b62a5abbccd5b2118498428b99874
Roy Barber <hi@roybarber.com>
2019-08-19 15:56:35 +0100

    Icon helper tweaks with color

--- gulp/handlebars.js
+++ gulp/handlebars.js
@@ -13,7 +13,7 @@ var jsonminify = require('gulp-jsonminify');
 var config = require('../build/build.config.js');
 
 /**
- * @param  {String} `path` The path to the SVG file to inline
+ * @param  {String} `icon` The path to the SVG file to inline
  * @param  {Object} `options`
  * @return {String}
  */
@@ -25,28 +25,48 @@ function parse (xml) {
     if (svg.name !== 'svg') {
         throw new TypeError('{{icon}} helper: File specified must be an SVG');
     }
-    delete svg.attrs.xmlns;
-    delete svg.attrs['xmlns:xlink'];
+    //delete svg.attrs.xmlns;
+    //delete svg.attrs['xmlns:xlink'];
     return svg;
 }
 const helpers = {
-	icon(path, options = {}){
-		var path = config.iconPath + path
-		if (typeof path !== 'string') {
+	icon(icon, options = {}){
+		var icon = config.iconPath + icon + '.svg';
+		if (typeof icon !== 'string') {
 			throw new Error('{{icon}} helper: invalid path. Path must be formatted as a string.');
 		}
 		let modulePath;
 		try {
-			modulePath = nameToModule[path] || (nameToModule[path] = resolve.sync(path, {
+			modulePath = nameToModule[icon] || (nameToModule[icon] = resolve.sync(icon, {
 				extensions: ['.svg']
 			}));
 		} catch (err) {
 			throw new Error(`{{icon}} helper: . ${err}`);
 		}
-		const content = cache[path] || (cache[path] = fs.readFileSync(modulePath, 'utf-8'));
+		const content = cache[icon] || (cache[icon] = fs.readFileSync(modulePath, 'utf-8'));
 		const svg = parse(content);
 		Object.assign(svg.attrs, options.hash);
 		return new Handlebars.SafeString(svg.root());
+	},
+	iconBackground(icon, options = {}){
+		var icon = config.iconPath + icon + '.svg';
+		if (typeof icon !== 'string') {
+			throw new Error('{{icon}} helper: invalid path. Path must be formatted as a string.');
+		}
+		let modulePath;
+		try {
+			modulePath = nameToModule[icon] || (nameToModule[icon] = resolve.sync(icon, {
+				extensions: ['.svg']
+			}));
+		} catch (err) {
+			throw new Error(`{{icon}} helper: . ${err}`);
+		}
+		const content = cache[icon] || (cache[icon] = fs.readFileSync(modulePath, 'utf-8'));
+		const svg = parse(content);
+		Object.assign(svg.attrs, options.hash);
+		const svgencoded = Buffer.from(String(svg)).toString('base64');
+		const output = "<div class='icon icon-background " + options.hash.class + "' style='background-image: url(data:image/svg+xml;base64," + svgencoded + ")'></div>";
+		return new Handlebars.SafeString(output);
 	}
 }
 
