7bee641403ffeac9589fe1ab5cc5c06b8df523ed
Roy Barber <hi@roybarber.com>
2021-11-15 11:18:53 +0000

    Add all plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
 		extend: {},
 	},
 	plugins: [
-		require('@tailwindcss/forms')
-	],
+		require('@tailwindcss/forms'),
+		require('@tailwindcss/typography'),
+      	require('@tailwindcss/aspect-ratio'),
+		require('@tailwindcss/line-clamp')
+	]
 }
