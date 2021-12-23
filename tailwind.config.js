b4d58c54d49f2295d59d6f99b08bf736b27d5ddd
Roy Barber <roy.barber@dotcentric.co.uk>
2021-12-23 14:00:46 +0000

    Latest

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
 module.exports = {
-	mode: 'jit',
-	purge: ['./src/**/*.html', './src/assets/js/**/*.js'],
-	darkMode: false, // or 'media' or 'class'
+	content: ["./src/**/*.{html,js}"],
 	theme: {
 		extend: {},
 	},
-	variants: {
-		extend: {},
-	},
 	plugins: [
+		require('@tailwindcss/typography')({
+			className: 'wysiwyg'
+		}),
 		require('@tailwindcss/forms')
-	],
+	]
 }
